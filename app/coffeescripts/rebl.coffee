5aa3979394380ccfd7f15418c8bbcc453e44fb92
ben@benatkin.com <ben@benatkin.com>
2010-10-05 06:39:52 -0600

    start writing dispatcher

--- app/coffeescripts/rebl.coffee
+++ app/coffeescripts/rebl.coffee
@@ -1,16 +1,60 @@
-class Resource
-  dispatch: (args...) ->
-    @call {}
-  call: (env) ->
+class Dispatcher
+  constructor: (root) ->
+    @current = @root = root
 
-class Rebl extends Resource
   call: (env) ->
-    "Welcome to rebl! For help, run $.rebl('help')."
+    if env.action and env.action.path
+      @current = @navigate(@current, env.action.path)
+    if not @current
+      return
+
+    new_env = @current.call(env)
+    if new_env and new_env.next
+      new_env.action = new_env.next
+      delete new_env['next']
+      @call(new_env)
+    new_env
+
+  navigate: (handler, path) ->
+    if path.length == 0 or not handler
+      return handler
+    name = path[0]
+    if name == '..'
+      @navigate handler.parent, path.slice(1)
+    else
+      @navigate handler[name], path.slice(1)
+
+class Base
+  constructor: (parent) ->
+    @parent = parent
+
+  call: (env) ->
+    if env.action && env.action.method and _.isFunction(this[env.action.method])
+      this[env.action.method](env)
+
+  next: (env, action) ->
+    env.next = action
+    env
+
+class Widget extends Base
+  build: (env) ->
+    $('<textarea>').appendTo(env.selector)
+
+class Rebl extends Base
+  constructor: () ->
+    @widget = new Widget(this)
+    super
+
+  call: (env) ->
+    if env.selector
+      @next(env, {'path': ['widget'], 'method': 'build'})
+    else if _.size(env.args) == 0
+      "Welcome to rebl! For help, run $.rebl({help: true})."
 
 $.Rebl = new Rebl
 
 $.rebl = (args...) ->
-  $.Rebl.dispatch(args...)
+  new Dispatcher($.Rebl).call({args: args})
 
 $::rebl = (args...) ->
-  $.Rebl.dispatch(args..., {selector: this})
+  new Dispatcher($.Rebl).call({selector: this, args: args})
--- public/javascripts/rebl.js
+++ public/javascripts/rebl.js
@@ -1,5 +1,5 @@
 (function() {
-  var Rebl, Resource;
+  var Base, Dispatcher, Rebl, Widget;
   var __extends = function(child, parent) {
     var ctor = function(){};
     ctor.prototype = parent.prototype;
@@ -8,29 +8,82 @@
     if (typeof parent.extended === "function") parent.extended(child);
     child.__super__ = parent.prototype;
   }, __slice = Array.prototype.slice;
-  Resource = function() {};
-  Resource.prototype.dispatch = function() {
-    return this.call({});
+  Dispatcher = function(root) {
+    this.current = (this.root = root);
+    return this;
   };
-  Resource.prototype.call = function(env) {};
+  Dispatcher.prototype.call = function(env) {
+    var new_env;
+    if (env.action && env.action.path) {
+      this.current = this.navigate(this.current, env.action.path);
+    }
+    if (!this.current) {
+      return null;
+    }
+    new_env = this.current.call(env);
+    if (new_env && new_env.next) {
+      new_env.action = new_env.next;
+      delete new_env['next'];
+      this.call(new_env);
+    }
+    return new_env;
+  };
+  Dispatcher.prototype.navigate = function(handler, path) {
+    var name;
+    if (path.length === 0 || !handler) {
+      return handler;
+    }
+    name = path[0];
+    return name === '..' ? this.navigate(handler.parent, path.slice(1)) : this.navigate(handler[name], path.slice(1));
+  };
+  Base = function(parent) {
+    this.parent = parent;
+    return this;
+  };
+  Base.prototype.call = function(env) {
+    return env.action && env.action.method && _.isFunction(this[env.action.method]) ? this[env.action.method](env) : null;
+  };
+  Base.prototype.next = function(env, action) {
+    env.next = action;
+    return env;
+  };
+  Widget = function() {
+    return Base.apply(this, arguments);
+  };
+  __extends(Widget, Base);
+  Widget.prototype.build = function(env) {
+    return $('<textarea>').appendTo(env.selector);
+  };
   Rebl = function() {
-    return Resource.apply(this, arguments);
+    this.widget = new Widget(this);
+    Rebl.__super__.constructor.apply(this, arguments);
+    return this;
   };
-  __extends(Rebl, Resource);
+  __extends(Rebl, Base);
   Rebl.prototype.call = function(env) {
-    return "Welcome to rebl! For help, run $.rebl('help').";
+    if (env.selector) {
+      return this.next(env, {
+        'path': ['widget'],
+        'method': 'build'
+      });
+    } else if (_.size(env.args) === 0) {
+      return "Welcome to rebl! For help, run $.rebl({help: true}).";
+    }
   };
   $.Rebl = new Rebl();
   $.rebl = function() {
     var args;
     args = __slice.call(arguments, 0);
-    return $.Rebl.dispatch.apply($.Rebl, args);
+    return new Dispatcher($.Rebl).call({
+      args: args
+    });
   };
   $.prototype.rebl = function() {
     var args;
     args = __slice.call(arguments, 0);
-    return $.Rebl.dispatch.apply($.Rebl, args.concat([{
-      selector: this
-    }]));
+    return new Dispatcher($.Rebl).call({
+      selector: this,
+      args: args
+    });
   };
 }).call(this);
